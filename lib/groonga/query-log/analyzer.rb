22b34f2bb5ba17f8fa888f2ca2cea46fb6a1a82c
Haruka Yoshihara <yoshihara@clear-code.com>
2012-12-12 15:43:06 +0900

    Move parsers under Groonga::QueryLog::Analyzer

--- lib/groonga/query-log/analyzer.rb
+++ lib/groonga/query-log/analyzer.rb
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 require 'optparse'
-require "groonga/query-log/reporter"
+require "groonga/query-log/analyzer/reporter"
 
 module Groonga
   module QueryLog
