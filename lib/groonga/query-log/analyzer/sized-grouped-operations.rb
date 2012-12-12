c068cda3f63c25e69da9a6a5ec3cda33ff8c9204
Haruka Yoshihara <yoshihara@clear-code.com>
2012-12-12 16:28:00 +0900

    sized-grouped-operations: indent

--- lib/groonga/query-log/analyzer/sized-grouped-operations.rb
+++ lib/groonga/query-log/analyzer/sized-grouped-operations.rb
@@ -20,65 +20,65 @@
 module Groonga
   module QueryLog
     class Analyzer
-    class SizedGroupedOperations < Array
-      def initialize
-        @max_size = 10
-        @sorter = create_sorter
-      end
-
-      def apply_options(options)
-        @max_size = options[:n_entries]
-      end
-
-      def each
-        i = 0
-        super do |grouped_operation|
-          break if i >= @max_size
-          i += 1
-          yield(grouped_operation)
+      class SizedGroupedOperations < Array
+        def initialize
+          @max_size = 10
+          @sorter = create_sorter
         end
-      end
 
-      def <<(operation)
-        each do |grouped_operation|
-          if grouped_operation[:name] == operation[:name] and
-              grouped_operation[:context] == operation[:context]
-            elapsed = operation[:relative_elapsed_in_seconds]
-            grouped_operation[:total_elapsed] += elapsed
-            grouped_operation[:n_operations] += 1
-            replace(sort_by(&@sorter))
-            return self
+        def apply_options(options)
+          @max_size = options[:n_entries]
+        end
+
+        def each
+          i = 0
+          super do |grouped_operation|
+            break if i >= @max_size
+            i += 1
+            yield(grouped_operation)
           end
         end
 
-        grouped_operation = {
-          :name => operation[:name],
-          :context => operation[:context],
-          :n_operations => 1,
-          :total_elapsed => operation[:relative_elapsed_in_seconds],
-        }
-        buffer_size = @max_size * 100
-        if size < buffer_size
-          super(grouped_operation)
-          replace(sort_by(&@sorter))
-        else
-          if @sorter.call(grouped_operation) < @sorter.call(last)
+        def <<(operation)
+          each do |grouped_operation|
+            if grouped_operation[:name] == operation[:name] and
+                grouped_operation[:context] == operation[:context]
+              elapsed = operation[:relative_elapsed_in_seconds]
+              grouped_operation[:total_elapsed] += elapsed
+              grouped_operation[:n_operations] += 1
+              replace(sort_by(&@sorter))
+              return self
+            end
+          end
+
+          grouped_operation = {
+            :name => operation[:name],
+            :context => operation[:context],
+            :n_operations => 1,
+            :total_elapsed => operation[:relative_elapsed_in_seconds],
+          }
+          buffer_size = @max_size * 100
+          if size < buffer_size
             super(grouped_operation)
-            sorted_operations = sort_by(&@sorter)
-            sorted_operations.pop
-            replace(sorted_operations)
+            replace(sort_by(&@sorter))
+          else
+            if @sorter.call(grouped_operation) < @sorter.call(last)
+              super(grouped_operation)
+              sorted_operations = sort_by(&@sorter)
+              sorted_operations.pop
+              replace(sorted_operations)
+            end
+          end
+          self
+        end
+
+        private
+        def create_sorter
+          lambda do |grouped_operation|
+            -grouped_operation[:total_elapsed]
           end
         end
-        self
       end
-
-      private
-      def create_sorter
-        lambda do |grouped_operation|
-          -grouped_operation[:total_elapsed]
-        end
-      end
-    end
     end
   end
 end
