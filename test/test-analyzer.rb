1b7ab2a847911ab0e629f2ea29556379dde72a80
Haruka Yoshihara <yoshihara@clear-code.com>
2012-12-13 18:02:45 +0900

    test: remove needless require

--- test/test-analyzer.rb
+++ test/test-analyzer.rb
@@ -16,7 +16,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "stringio"
 require "tempfile"
 require "pathname"
 require "groonga/query-log/analyzer"
