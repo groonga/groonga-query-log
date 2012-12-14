fa6b1bdb5ec1a04044720c1e40b26c28c18db228
Haruka Yoshihara <yoshihara@clear-code.com>
2012-12-14 17:32:03 +0900

    extractor: implement #initialize
    It doesn't work yet.

--- lib/groonga/query-log/extractor.rb
+++ lib/groonga/query-log/extractor.rb
@@ -26,6 +26,75 @@ require "groonga/query-log"
 module Groonga
   module QueryLog
     class Extractor
+
+      class Error < StandardError
+      end
+
+      class NoInputError < Error
+      end
+
+      attr_accessor :options
+      attr_reader :option_parser
+
+      def initialize
+        @options = nil
+        @option_parser = nil
+        setup_options
+      end
+
+      def setup_options
+        @options = OpenStruct.new
+        @options.unify_format = nil
+        @options.commands = []
+        @options.exclude_commands = []
+        @options.output_path = nil
+        @option_parser = OptionParser.new do |parser|
+          parser.banner += " QUERY_LOG1 ..."
+
+          available_formats = ["uri", "command"]
+          parser.on("--unify-format=FORMAT",
+                 available_formats,
+                 "Unify command format to FORMAT.",
+                 "(#{available_formats.join(', ')})",
+                 "[not unify]") do |format|
+            @options.unify_format = format
+          end
+
+          parser.on("--command=COMMAND",
+                 "Extract only COMMAND.",
+                 "To extract one or more commands,",
+                 "specify this command a number of times.",
+                 "Use /.../ as COMMAND to match command with regular expression.",
+                 "[all commands]") do |command|
+            case command
+            when /\A\/(.*)\/(i)?\z/
+              @options.commands << Regexp.new($1, $2 == "i")
+            when
+              @options.commands << command
+            end
+          end
+
+          parser.on("--exclude-command=COMMAND",
+                 "Don't extract COMMAND.",
+                 "To ignore one or more commands,",
+                 "specify this command a number of times.",
+                 "Use /.../ as COMMAND to match command with regular expression.",
+                 "[no commands]") do |command|
+            case command
+            when /\A\/(.*)\/(i)?\z/
+              @options.exclude_commands << Regexp.new($1, $2 == "i")
+            when
+              @options.exclude_commands << command
+            end
+          end
+
+          parser.on("--output=PATH",
+                 "Output to PATH.",
+                 "[standard output]") do |path|
+            @options.output_path = path
+          end
+        end
+      end
     end
   end
 end
