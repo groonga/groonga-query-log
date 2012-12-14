8e89dd9f9106b8ffe1ac0e653aa69961fabc79a5
Haruka Yoshihara <yoshihara@clear-code.com>
2012-12-14 17:00:15 +0900

    Add Groonga::QueryLog::Extractor Class
    It's going to be implemented from bin/groonga-query-log-extract.

--- lib/groonga/query-log/extractor.rb
+++ lib/groonga/query-log/extractor.rb
@@ -1,11 +1,12 @@
+#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2011  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012  Haruka Yoshihara <yoshihara@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
+# License version 2.1 as published by the Free Software Foundation.
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,9 +15,17 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "groonga/query-log/version"
-require "groonga/query-log/analyzer"
-require "groonga/query-log/extractor"
-require "groonga/query-log/parser"
+require "ostruct"
+require "optparse"
+require "pathname"
+
+require "groonga/query-log"
+
+module Groonga
+  module QueryLog
+    class Extractor
+    end
+  end
+end
