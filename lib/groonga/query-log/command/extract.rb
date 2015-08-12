be48d65b53ee9f15d573093fc600159f5be5b342
Kouhei Sutou <kou@clear-code.com>
2015-08-12 18:37:28 +0900

    Indent

--- lib/groonga/query-log/command/extract.rb
+++ lib/groonga/query-log/command/extract.rb
@@ -26,167 +26,167 @@ require "groonga/query-log/command-line-utils"
 module Groonga
   module QueryLog
     module Command
-    class Extract
-      include CommandLineUtils
+      class Extract
+        include CommandLineUtils
 
-      class Error < StandardError
-      end
-
-      attr_accessor :options
-      attr_reader :option_parser
-
-      def initialize
-        @options = nil
-        @option_parser = nil
-        setup_options
-      end
-
-      # Executes extractor for groonga's query logs.
-      # "groonga-query-log-extract" command runs this method.
-      #
-      # @example
-      #   extractor = Groonga::QueryLog::Command::Extract.new
-      #   extractor.run("--output", "commands.output",
-      #                 "--command", "select",
-      #                 "query.log")
-      #
-      # If only paths of query log files are specified,
-      # this method prints command(s) of them to console.
-      #
-      # @param [Array<String>] arguments arguments for
-      #   groonga-query-log-extract. Please execute
-      #   "groonga-query-log-extract --help" or see #setup_options.
-      def run(arguments)
-        begin
-          log_paths = @option_parser.parse!(arguments)
-        rescue OptionParser::ParseError
-          $stderr.puts($!.message)
-          return false
+        class Error < StandardError
         end
 
-        if log_paths.empty?
-          unless log_via_stdin?
-            $stderr.puts("Error: Please specify input log files.")
+        attr_accessor :options
+        attr_reader :option_parser
+
+        def initialize
+          @options = nil
+          @option_parser = nil
+          setup_options
+        end
+
+        # Executes extractor for groonga's query logs.
+        # "groonga-query-log-extract" command runs this method.
+        #
+        # @example
+        #   extractor = Groonga::QueryLog::Command::Extract.new
+        #   extractor.run("--output", "commands.output",
+        #                 "--command", "select",
+        #                 "query.log")
+        #
+        # If only paths of query log files are specified,
+        # this method prints command(s) of them to console.
+        #
+        # @param [Array<String>] arguments arguments for
+        #   groonga-query-log-extract. Please execute
+        #   "groonga-query-log-extract --help" or see #setup_options.
+        def run(arguments)
+          begin
+            log_paths = @option_parser.parse!(arguments)
+          rescue OptionParser::ParseError
+            $stderr.puts($!.message)
             return false
           end
-          log = $stdin
-        else
-          log = log_paths
-        end
 
-        if @options.output_path
-          File.open(@options.output_path, "w") do |output|
-            extract(log, output)
-          end
-        else
-          extract(log, $stdout)
-        end
-
-        true
-      end
-
-      private
-      def setup_options
-        @options = OpenStruct.new
-        @options.unify_format = nil
-        @options.commands = []
-        @options.exclude_commands = []
-        @options.output_path = nil
-        @option_parser = OptionParser.new do |parser|
-          parser.version = VERSION
-          parser.banner += " QUERY_LOG1 ..."
-
-          available_formats = ["uri", "command"]
-          parser.on("--unify-format=FORMAT",
-                    available_formats,
-                    "Unify command format to FORMAT.",
-                    "(#{available_formats.join(', ')})",
-                    "[not unify]") do |format|
-            @options.unify_format = format
-          end
-
-          parser.on("--command=COMMAND",
-                    "Extract only COMMAND.",
-                    "To extract one or more commands,",
-                    "specify this command a number of times.",
-                    "Use /.../ as COMMAND to match command with regular expression.",
-                    "[all commands]") do |command|
-            case command
-            when /\A\/(.*)\/(i)?\z/
-              @options.commands << Regexp.new($1, $2 == "i")
-            when
-              @options.commands << command
+          if log_paths.empty?
+            unless log_via_stdin?
+              $stderr.puts("Error: Please specify input log files.")
+              return false
             end
-          end
-
-          parser.on("--exclude-command=COMMAND",
-                    "Don't extract COMMAND.",
-                    "To ignore one or more commands,",
-                    "specify this command a number of times.",
-                    "Use /.../ as COMMAND to match command with regular expression.",
-                    "[no commands]") do |command|
-            case command
-            when /\A\/(.*)\/(i)?\z/
-              @options.exclude_commands << Regexp.new($1, $2 == "i")
-            when
-              @options.exclude_commands << command
-            end
-          end
-
-          parser.on("--output=PATH",
-                    "Output to PATH.",
-                    "[standard output]") do |path|
-            @options.output_path = path
-          end
-        end
-      end
-
-      def extract(log, output)
-        if log.instance_of?(Array)
-          log.each do |log_path|
-            File.open(log_path) do |log_file|
-              extract_command(log_file, output)
-            end
-          end
-        else
-          extract_command(log, output)
-        end
-      end
-
-      def extract_command(log, output)
-        parser = Groonga::QueryLog::Parser.new
-        parser.parse(log) do |statistic|
-          command = statistic.command
-          next unless target?(command)
-          command_text = nil
-          case @options.unify_format
-          when "uri"
-            command_text = command.to_uri_format
-          when "command"
-            command_text = command.to_command_format
+            log = $stdin
           else
-            command_text = statistic.raw_command
+            log = log_paths
           end
-          output.puts(command_text)
+
+          if @options.output_path
+            File.open(@options.output_path, "w") do |output|
+              extract(log, output)
+            end
+          else
+            extract(log, $stdout)
+          end
+
+          true
+        end
+
+        private
+        def setup_options
+          @options = OpenStruct.new
+          @options.unify_format = nil
+          @options.commands = []
+          @options.exclude_commands = []
+          @options.output_path = nil
+          @option_parser = OptionParser.new do |parser|
+            parser.version = VERSION
+            parser.banner += " QUERY_LOG1 ..."
+
+            available_formats = ["uri", "command"]
+            parser.on("--unify-format=FORMAT",
+                      available_formats,
+                      "Unify command format to FORMAT.",
+                      "(#{available_formats.join(', ')})",
+                      "[not unify]") do |format|
+              @options.unify_format = format
+            end
+
+            parser.on("--command=COMMAND",
+                      "Extract only COMMAND.",
+                      "To extract one or more commands,",
+                      "specify this command a number of times.",
+                      "Use /.../ as COMMAND to match command with regular expression.",
+                      "[all commands]") do |command|
+              case command
+              when /\A\/(.*)\/(i)?\z/
+                @options.commands << Regexp.new($1, $2 == "i")
+              when
+                @options.commands << command
+              end
+            end
+
+            parser.on("--exclude-command=COMMAND",
+                      "Don't extract COMMAND.",
+                      "To ignore one or more commands,",
+                      "specify this command a number of times.",
+                      "Use /.../ as COMMAND to match command with regular expression.",
+                      "[no commands]") do |command|
+              case command
+              when /\A\/(.*)\/(i)?\z/
+                @options.exclude_commands << Regexp.new($1, $2 == "i")
+              when
+                @options.exclude_commands << command
+              end
+            end
+
+            parser.on("--output=PATH",
+                      "Output to PATH.",
+                      "[standard output]") do |path|
+              @options.output_path = path
+            end
+          end
+        end
+
+        def extract(log, output)
+          if log.instance_of?(Array)
+            log.each do |log_path|
+              File.open(log_path) do |log_file|
+                extract_command(log_file, output)
+              end
+            end
+          else
+            extract_command(log, output)
+          end
+        end
+
+        def extract_command(log, output)
+          parser = Groonga::QueryLog::Parser.new
+          parser.parse(log) do |statistic|
+            command = statistic.command
+            next unless target?(command)
+            command_text = nil
+            case @options.unify_format
+            when "uri"
+              command_text = command.to_uri_format
+            when "command"
+              command_text = command.to_command_format
+            else
+              command_text = statistic.raw_command
+            end
+            output.puts(command_text)
+          end
+        end
+
+        def target?(command)
+          name = command.name
+          target_commands = @options.commands
+          exclude_commands = @options.exclude_commands
+
+          unless target_commands.empty?
+            return target_commands.any? {|target_command| target_command === name}
+          end
+
+          unless exclude_commands.empty?
+            return (not exclude_commands.any? {|exclude_command| exclude_command === name})
+          end
+
+          true
         end
       end
-
-      def target?(command)
-        name = command.name
-        target_commands = @options.commands
-        exclude_commands = @options.exclude_commands
-
-        unless target_commands.empty?
-          return target_commands.any? {|target_command| target_command === name}
-        end
-
-        unless exclude_commands.empty?
-          return (not exclude_commands.any? {|exclude_command| exclude_command === name})
-        end
-
-        true
-      end
-    end
     end
   end
 end
