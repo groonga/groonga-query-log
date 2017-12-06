e8fa8cdfb1817cf2e3ea8c5df0981e03bf1571ab
Kouhei Sutou <kou@clear-code.com>
2017-12-06 11:05:14 +0900

    Unindent

--- lib/groonga-query-log/command/replay.rb
+++ lib/groonga-query-log/command/replay.rb
@@ -20,104 +20,104 @@ require "groonga-query-log/version"
 require "groonga-query-log/replayer"
 
 module GroongaQueryLog
-    module Command
-      class Replay
-        def initialize
-          @options = Replayer::Options.new
+  module Command
+    class Replay
+      def initialize
+        @options = Replayer::Options.new
+      end
+
+      def run(command_line)
+        input_paths = create_parser.parse(command_line)
+        replayer = Replayer.new(@options)
+        input_paths.each do |input_path|
+          File.open(input_path) do |input|
+            replayer.replay(input)
+          end
+        end
+        true
+      end
+
+      private
+      def create_parser
+        parser = OptionParser.new
+        parser.version = VERSION
+        parser.banner += " QUERY_LOG"
+
+        parser.separator("")
+        parser.separator("Options:")
+
+        parser.on("--host=HOST",
+                  "Host name or IP address of Groonga server",
+                  "[#{@options.host}]") do |host|
+          @options.host = host
         end
 
-        def run(command_line)
-          input_paths = create_parser.parse(command_line)
-          replayer = Replayer.new(@options)
-          input_paths.each do |input_path|
-            File.open(input_path) do |input|
-              replayer.replay(input)
-            end
-          end
-          true
+        parser.on("--port=PORT", Integer,
+                  "Port number of Groonga server",
+                  "[#{@options.port}]") do |port|
+          @options.port = port
         end
 
-        private
-        def create_parser
-          parser = OptionParser.new
-          parser.version = VERSION
-          parser.banner += " QUERY_LOG"
+        available_protocols = [:gqtp, :http]
+        available_protocols_label = "(#{available_protocols.join(', ')})"
+        parser.on("--protocol=PROTOCOL", available_protocols,
+                  "Protocol of Groonga server",
+                  "[#{@options.protocol}]",
+                  available_protocols_label) do |protocol|
+          @options.protocol = protocol
+        end
 
-          parser.separator("")
-          parser.separator("Options:")
+        parser.on("--n-clients=N", Integer,
+                  "The max number of concurrency",
+                  "[#{@options.n_clients}]") do |n_clients|
+          @options.n_clients = n_clients
+        end
 
-          parser.on("--host=HOST",
-                    "Host name or IP address of Groonga server",
-                    "[#{@options.host}]") do |host|
-            @options.host = host
-          end
+        parser.on("--request-queue-size=SIZE", Integer,
+                  "The size of request queue",
+                  "[auto]") do |size|
+          @options.request_queue_size = size
+        end
 
-          parser.on("--port=PORT", Integer,
-                    "Port number of Groonga server",
-                    "[#{@options.port}]") do |port|
-            @options.port = port
-          end
+        parser.on("--disable-cache",
+                  "Add 'cache=no' parameter to request",
+                  "[#{@options.disable_cache?}]") do
+          @options.disable_cache = true
+        end
 
-          available_protocols = [:gqtp, :http]
-          available_protocols_label = "(#{available_protocols.join(', ')})"
-          parser.on("--protocol=PROTOCOL", available_protocols,
-                    "Protocol of Groonga server",
-                    "[#{@options.protocol}]",
-                    available_protocols_label) do |protocol|
-            @options.protocol = protocol
-          end
+        parser.on("--target-command-name=NAME",
+                  "Add NAME to target command names",
+                  "You can specify this option zero or more times",
+                  "See also --target-command-names") do |name|
+          @options.target_command_names << name
+        end
 
-          parser.on("--n-clients=N", Integer,
-                    "The max number of concurrency",
-                    "[#{@options.n_clients}]") do |n_clients|
-            @options.n_clients = n_clients
-          end
+        target_command_names_label = @options.target_command_names.join(", ")
+        parser.on("--target-command-names=NAME1,NAME2,...", Array,
+                  "Replay only NAME1,NAME2,... commands",
+                  "You can use glob to choose command name",
+                  "[#{target_command_names_label}]") do |names|
+          @options.target_command_names = names
+        end
 
-          parser.on("--request-queue-size=SIZE", Integer,
-                    "The size of request queue",
-                    "[auto]") do |size|
-            @options.request_queue_size = size
-          end
+        parser.on("--output-requests=PATH",
+                  "Output requests to PATH",
+                  "[not output]") do |path|
+          @options.requests_path = path
+        end
 
-          parser.on("--disable-cache",
-                    "Add 'cache=no' parameter to request",
-                    "[#{@options.disable_cache?}]") do
-            @options.disable_cache = true
-          end
+        parser.on("--output-responses=PATH",
+                  "Output responses to PATH",
+                  "[not output]") do |path|
+          @options.responses_path = path
+        end
 
-          parser.on("--target-command-name=NAME",
-                    "Add NAME to target command names",
-                    "You can specify this option zero or more times",
-                    "See also --target-command-names") do |name|
-            @options.target_command_names << name
-          end
-
-          target_command_names_label = @options.target_command_names.join(", ")
-          parser.on("--target-command-names=NAME1,NAME2,...", Array,
-                    "Replay only NAME1,NAME2,... commands",
-                    "You can use glob to choose command name",
-                    "[#{target_command_names_label}]") do |names|
-            @options.target_command_names = names
-          end
-
-          parser.on("--output-requests=PATH",
-                    "Output requests to PATH",
-                    "[not output]") do |path|
-            @options.requests_path = path
-          end
-
-          parser.on("--output-responses=PATH",
-                    "Output responses to PATH",
-                    "[not output]") do |path|
-            @options.responses_path = path
-          end
-
-          parser.on("--ignore-error",
-                    "Ignore error",
-                    "[#{@options.ignore_error?}]") do
-            @options.ignore_error = true
-          end
+        parser.on("--ignore-error",
+                  "Ignore error",
+                  "[#{@options.ignore_error?}]") do
+          @options.ignore_error = true
         end
       end
     end
+  end
 end
