24da9ae76dfb1d709e3b99ff32dc8b4dbe9b5456
Kouhei Sutou <kou@clear-code.com>
2018-02-04 21:54:23 +0900

    Bump version

--- lib/groonga-query-log/version.rb
+++ lib/groonga-query-log/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2017  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012-2018  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,5 +15,5 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module GroongaQueryLog
-    VERSION = "1.2.9"
+  VERSION = "1.3.0"
 end
