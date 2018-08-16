da077052dbe06c7d319dc20ccd45a97891233e5a
Kouhei Sutou <kou@clear-code.com>
2018-08-16 14:37:15 +0900

    extract: add missing require

--- lib/groonga-query-log/command/extract.rb
+++ lib/groonga-query-log/command/extract.rb
@@ -14,8 +14,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "ostruct"
+require "json"
 require "optparse"
+require "ostruct"
 require "pathname"
 
 require "groonga-query-log"
