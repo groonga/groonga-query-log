36f6610ce5da4fedd574aec98ab89aac844158ca
Kentaro Hayashi <hayashi@clear-code.com>
2019-08-09 16:08:12 +0900

    check-performance-regression: detect performance regression (#40)
    * check-performance-regression: detect performance regression
    
    * check-performance-regression: use elapsed_ratio to avoid division by zero
    
    * check-performance-regression: loose skip to extra statistics
    
    * check-performance-regression: add option to filter query
    
    * check-performance-regression: use nanoseconds in average
    
    * check-performance-regression: follow format change
    
    * check-performance-regression: count each queries for summary
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    * check-performance-regression: skip same result
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    * check-performance-regression: show slow context to distinguish
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    * check-performance-regression: follow format change
    
    * check-performance-regression: fix inappropriate author
    
    * check-performance-regression: use proper plurals forms (n_XXX)
    
    * check-performance-regression: change query log path as required parameter
    
    * check-performance-regression: follow changed required parameter test case
    
    * check-performance-regression: follow changed required parameter test case
    
    * check-performance-regression: use is_a? for generic object orientation
    
    * check-performance-regression: drop needless +
    
    Because output is not read during checking.
    
    * check-performance-regression: use sort_by for performance
    
    * check-performance-regression: simplify condition
    
    * check-performance-regression: fix unexpected shadowing
    
    * check-performance-regression: use each_key for straightforward
    
    * check-performance-regression: remove needless assign
    
    * check-performance-regression: use double quote
    
    * check-performance-regression: fix a typo
    
    * check-performance-regression: use File#readlines
    
    * check-performance-regression: use unless for simplicity
    
    * check-performance-regression: use #zip for simplicity
    
    * check-performance-regression: use ||= [] idiom
    
    * check-performance-regression: simplify if ... else ...
    
    * check-performance-regression: use more meaningful option name
    
    * check-performance-regression: remove needless assignment
    
    * check-performance-regression: remove useless description
    
    * check-performance-regression: simplify
    
    * check-performance-regression: limit N entries
    
    * check-performance-regression: remove needless coding
    
    * check-performance-regression: remove needless empty line
    
    * check-performance-regression: use StringIO
    
    * check-performance-regression: use Tempfile for simplicity
    
    * check-performance-regression: align result
    
    * check-performance-regression: align test result
    
    * check-performance-regression: simplify cached_query?
    
    * check-performance-regression: simplify :name access
    
    * check-performance-regression: use meaningful label
    
    * check-performance-regression: fix label
    
    * check-performance-regression: use open_output() for simplicity
    
    * check-performance-regression: fix argument error on Ruby2.2
    
    * check-performance-regression: remove unused constant
    
    * check-performance-regression: use constant for readability
    
    * check-performance-regression: use meaningful option name
    
    * Don't reuse options not for options
    
    * Add missing error checks for analyze
    
    * Remove needless method
    
    * Simplify
    
    * Add missing "s"
    
    * Fix indent
    
    * Simplify
    
    * Don't use options not for variable used in the class
    
    * Split to classes
    
    * Unify common code

---
+++ lib/groonga-query-log/command/check-performance-regression.rb
@@ -0,0 +1,451 @@
+# Copyright (C) 2019 Kentaro Hayashi <hayashi@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "optparse"
+require "json"
+
+require "groonga-query-log"
+require "groonga-query-log/command-line"
+
+require "groonga-query-log/command/analyzer"
+require "groonga-query-log/command/analyzer/sized-statistics"
+
+module GroongaQueryLog
+  module Command
+    class CheckPerformanceRegression < CommandLine
+      NSEC_IN_SECONDS = (1000 * 1000 * 1000)
+      USEC_IN_SECONDS = (1000 * 1000)
+      MSEC_IN_SECONDS = 1000
+
+      def initialize(options={})
+        setup_options(options)
+      end
+
+      def run(arguments)
+        paths = []
+        begin
+          paths = @option_parser.parse!(arguments)
+        rescue OptionParser::InvalidOption => error
+          $stderr.puts(error)
+          return false
+        end
+
+        if paths.size != 2
+          $stderr.puts("old query log and new query log must be specified.")
+          return false
+        end
+
+        old_query_paths = resolve_path(paths[0])
+        return false if old_query_paths.nil?
+        new_query_paths = resolve_path(paths[1])
+        return false if new_query_paths.nil?
+
+        old_statistics = analyze(old_query_paths)
+        return false if old_statistics.nil?
+        new_statistics = analyze(new_query_paths)
+        return false if new_statistics.nil?
+
+        open_output do |output|
+          checker = Checker.new(old_statistics,
+                                new_statistics,
+                                output,
+                                @threshold,
+                                @target_queries)
+          checker.check
+        end
+      end
+
+      private
+      def resolve_path(path)
+        if File.directory?(path)
+          Dir.glob("#{path}/*.log")
+        elsif File.exist?(path)
+          [path]
+        else
+          $stderr.puts("query log path doesn't exist: <#{path}>")
+          nil
+        end
+      end
+
+      def open_output(&block)
+        case @output
+        when "-"
+          yield($stdout)
+        when String
+          File.open(@output, "w", &block)
+        else
+          yield(@output)
+        end
+      end
+
+      def setup_options(options)
+        @output = options[:output] || "-"
+        @n_entries = 1000
+        @threshold = Threshold.new
+        @target_queries = []
+
+        @option_parser = OptionParser.new do |parser|
+          parser.version = VERSION
+          parser.banner += " OLD_QUERY_LOG NEW_QUERY_LOG"
+
+          parser.on("-n", "--n-entries=N",
+                    Integer,
+                    "Analyze N query log entries",
+                    "(#{@n_entries})") do |n|
+            @n_entries = n
+          end
+
+          if @output == "-"
+            default_output = "stdout"
+          else
+            default_output = @output
+          end
+          parser.on("--output=PATH",
+                    "Output to PATH.",
+                    "(#{default_output})") do |output|
+            @output = output
+          end
+
+          parser.on("--target-query-file=TARGET_QUERY_FILE",
+                    "Analyze matched queries which are listed " +
+                    "in specified TARGET_QUERY_FILE.") do |path|
+            if File.exist?(path)
+              @target_queries = File.readlines(path, chomp: true)
+            else
+              message = "target query file doesn't exist: <#{path}>"
+              raise OptionParser::InvalidOption.new(message)
+            end
+          end
+
+          parser.on("--slow-query-ratio=RATIO",
+                    Float,
+                    "Use RATIO as threshold to detect slow queries.",
+                    "If MEAN_NEW_ELAPSED_TIME / MEAN_OLD_ELAPSED_TIME AVERAGE",
+                    "is larger than RATIO, the query is slow.",
+                    "(#{@threshold.query_ratio})") do |ratio|
+            @threshold.query_ratio = ratio
+          end
+
+          parser.on("--slow-query-second=SECOND",
+                    Float,
+                    "Use SECOND as threshold to detect slow queries.",
+                    "If MEAN_NEW_ELAPSED_TIME - MEAN_OLD_ELAPSED_TIME AVERAGE",
+                    "is larger than SECOND, the query is slow.",
+                    "(#{@threshold.query_second})") do |second|
+            @threshold.query_second = second
+          end
+
+          parser.on("--slow-operation-ratio=RATIO",
+                    Float,
+                    "Use RATIO as threshold to detect slow operations.",
+                    "If MEAN_NEW_ELAPSED_TIME / MEAN_OLD_ELAPSED_TIME AVERAGE",
+                    "is larger than RATIO, the operation is slow.",
+                    "(#{@threshold.operation_ratio})") do |ratio|
+            @threshold.operation_ratio = ratio
+          end
+
+          parser.on("--slow-operation-second=SECOND",
+                    Float,
+                    "Use SECOND as threshold to detect slow operations.",
+                    "If MEAN_NEW_ELAPSED_TIME - MEAN_OLD_ELAPSED_TIME AVERAGE",
+                    "is larger than SECOND, the operation is slow.",
+                    "(#{@threshold.operation_second})") do |second|
+            @threshold.operation_second = second
+          end
+        end
+      end
+
+
+      def analyze(log_paths)
+        full_statistics = []
+        begin
+          n = 1
+          parser = Parser.new
+          parse_log(parser, log_paths) do |statistic|
+            next if n > @n_entries
+            full_statistics << statistic
+            n += 1
+          end
+        rescue Error
+          $stderr.puts($!.message)
+          return nil
+        end
+        full_statistics
+      end
+
+      class Threshold
+        attr_accessor :query_ratio
+        attr_accessor :query_second
+        attr_accessor :operation_ratio
+        attr_accessor :operation_second
+        def initialize
+          @query_ratio = 0
+          @query_second = 0.2
+          @operation_ratio = 0.1
+          @operation_second = 0.1
+        end
+
+        def slow_query?(diff_sec, diff_ratio)
+          return false if diff_sec.zero?
+          (diff_sec >= @query_second) and
+            (diff_ratio >= @query_ratio)
+        end
+
+        def slow_operation?(diff_sec, diff_ratio)
+          return false if diff_sec.zero?
+          (diff_sec >= @operation_second) and
+            (diff_ratio >= @operation_ratio)
+        end
+      end
+
+      class Statistic
+        def initialize(old, new, threshold)
+          @old = old
+          @new = new
+          @threshold = threshold
+        end
+
+        def old_elapsed_time
+          @old_elapsed_time ||= compute_mean(@old)
+        end
+
+        def new_elapsed_time
+          @new_elapsed_time ||= compute_mean(@new)
+        end
+
+        def diff_elapsed_time
+          new_elapsed_time - old_elapsed_time
+        end
+
+        def ratio
+          @ratio ||= compute_ratio
+        end
+
+        private
+        def compute_ratio
+          if old_elapsed_time.zero?
+            if new_elapsed_time.zero?
+              0.0
+            else
+              Float::INIFINITY
+            end
+          else
+            new_elapsed_time / old_elapsed_time
+          end
+        end
+      end
+
+      class OperationStatistic < Statistic
+        attr_reader :index
+        def initialize(operation, index, old, new, threshold)
+          super(old, new, threshold)
+          @operation = operation
+          @index = index
+        end
+
+        def name
+          @operation[:name]
+        end
+
+        def context
+          @operation[:context]
+        end
+
+        def slow?
+          @threshold.slow_operation?(diff_elapsed_time, ratio)
+        end
+
+        private
+        def compute_mean(operations)
+          elapsed_times = operations.collect do |operation|
+            operation[:relative_elapsed] / 1000.0 / 1000.0 / 1000.0
+          end
+          elapsed_times.inject(:+) / elapsed_times.size
+        end
+      end
+
+      class QueryStatistic < Statistic
+        attr_reader :query
+        def initialize(query, old, new, threshold)
+          super(old, new, threshold)
+          @query = query
+        end
+
+        def slow?
+          @threshold.slow_query?(diff_elapsed_time, ratio)
+        end
+
+        def each_operation_statistic
+          @old.first.operations.each_with_index do |operation, i|
+            old_operations = @old.collect do |statistic|
+              statistic.operations[i]
+            end
+            # TODO: old and new may use different index
+            new_operations = @new.collect do |statistic|
+              statistic.operations[i]
+            end
+            operation_statistic = OperationStatistic.new(operation,
+                                                         i,
+                                                         old_operations,
+                                                         new_operations,
+                                                         @threshold)
+            yield(operation_statistic)
+          end
+        end
+
+        private
+        def compute_mean(statistics)
+          elapsed_times = statistics.collect do |statistic|
+            statistic.elapsed / 1000.0 / 1000.0 / 1000.0
+          end
+          elapsed_times.inject(:+) / elapsed_times.size
+        end
+      end
+
+      class Checker
+        def initialize(old_statistics,
+                       new_statistics,
+                       output,
+                       threshold,
+                       target_queries)
+          @old_statistics = old_statistics
+          @new_statistics = new_statistics
+          @output = output
+          @threshold = threshold
+          @target_queries = target_queries
+        end
+
+        def check
+          old_statistics = filter_statistics(@old_statistics)
+          new_statistics = filter_statistics(@new_statistics)
+          old_queries = old_statistics.group_by(&:raw_command)
+          new_queries = new_statistics.group_by(&:raw_command)
+
+          query_statistics = []
+          old_queries.each_key do |query|
+            query_statistic = QueryStatistic.new(query,
+                                                 old_queries[query],
+                                                 new_queries[query],
+                                                 @threshold)
+            next unless query_statistic.slow?
+            query_statistics << query_statistic
+          end
+
+          n_slow_queries = 0
+          n_target_operations = 0
+          n_slow_operations = 0
+          query_statistics.sort_by(&:ratio).each do |query_statistic|
+            n_slow_queries += 1
+            @output.puts(<<-REPORT)
+Query: #{query_statistic.query}
+  Mean (old): #{format_elapsed_time(query_statistic.old_elapsed_time)}
+  Mean (new): #{format_elapsed_time(query_statistic.new_elapsed_time)}
+  Diff:       #{format_diff(query_statistic)}
+  Operations:
+            REPORT
+            query_statistic.each_operation_statistic do |operation_statistic|
+              n_target_operations += 1
+              next unless operation_statistic.slow?
+
+              n_slow_operations += 1
+              index = operation_statistic.index
+              name = operation_statistic.name
+              context = operation_statistic.context
+              label = [name, context].compact.join(" ")
+              old_elapsed_time = operation_statistic.old_elapsed_time
+              new_elapsed_time = operation_statistic.new_elapsed_time
+              @output.puts(<<-REPORT)
+    Operation[#{index}]: #{label}
+      Mean (old): #{format_elapsed_time(old_elapsed_time)}
+      Mean (new): #{format_elapsed_time(new_elapsed_time)}
+      Diff:       #{format_diff(operation_statistic)}
+              REPORT
+            end
+          end
+
+          n_all_queries = @old_statistics.size
+          n_target_queries = old_queries.size
+          n_old_cached_queries = count_cached_queries(@old_statistics)
+          n_new_cached_queries = count_cached_queries(@new_statistics)
+          @output.puts(<<-REPORT)
+Summary:
+  Slow queries:    #{format_summary(n_slow_queries, n_target_queries)}
+  Slow operations: #{format_summary(n_slow_operations, n_target_operations)}
+  Caches (old):    #{format_summary(n_old_cached_queries, n_all_queries)}
+  Caches (new):    #{format_summary(n_new_cached_queries, n_all_queries)}
+          REPORT
+          true
+        end
+
+        private
+        def count_cached_queries(statistics)
+          n_cached_queries = 0
+          statistics.each do |statistic|
+            n_cached_queries += 1 if cached_query?(statistic)
+          end
+          n_cached_queries
+        end
+
+        def filter_statistics(statistics)
+          statistics.find_all do |statistic|
+            target_statistic?(statistic)
+          end
+        end
+
+        def cached_query?(statistics)
+          statistics.operations.collect { |operation| operation[:name] } == ["cache"]
+        end
+
+        def target_statistic?(statistic)
+          return false if cached_query?(statistic)
+          return true if @target_queries.empty?
+          @target_queries.include?(statistic.raw_command)
+        end
+
+        def format_elapsed_time(elapsed_time)
+          if elapsed_time < (1 / 1000.0 / 1000.0)
+            "%.1fnsec" % (elapsed_time * 1000 * 1000)
+          elsif elapsed_time < (1 / 1000.0)
+            "%.1fusec" % (elapsed_time * 1000 * 1000)
+          elsif elapsed_time < 1
+            "%.1fmsec" % (elapsed_time * 1000)
+          elsif elapsed_time < 60
+            "%.1fsec" % elapsed_time
+          else
+            "%.1fmin" % (elapsed_time / 60)
+          end
+        end
+
+        def format_diff(statistic)
+          "%s%s/%+.2f" % [
+            statistic.diff_elapsed_time < 0 ? "-" : "+",
+            format_elapsed_time(statistic.diff_elapsed_time),
+            statistic.ratio,
+          ]
+        end
+
+        def format_summary(n_slows, total)
+          if total.zero?
+            percentage = 0.0
+          else
+            percentage = (n_slows / total.to_f) * 100
+          end
+          "%d/%d(%6.2f%%)" % [n_slows, total, percentage]
+        end
+      end
+    end
+  end
+end
---
+++ test/command/test-check-performance-regression.rb
@@ -0,0 +1,311 @@
+# Copyright (C) 2019  Kentaro Hayashi <hayashi@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "groonga-query-log/command/check-performance-regression"
+
+class CheckPerformanceRegressionCommandTest < Test::Unit::TestCase
+  include Helper::Path
+
+  def setup
+    @command = GroongaQueryLog::Command::CheckPerformanceRegression.new
+  end
+
+  def fixture_path(*components)
+    super("check-performance-regression", *components)
+  end
+
+  def run_command(*arguments)
+    output = StringIO.new
+    options = {:output => output}
+    command = GroongaQueryLog::Command::CheckPerformanceRegression.new(options)
+    command.run(arguments)
+    output.string
+  end
+
+  sub_test_case("options") do
+    MISSING_QUERY_LOG_ERROR = <<-OUTPUT
+old query log and new query log must be specified.
+    OUTPUT
+
+    def run_command_with_stderr
+      begin
+        output = StringIO.new
+        $stderr = output
+        yield
+        output.string
+      ensure
+        $stderr = STDERR
+      end
+    end
+
+    def test_no_option
+      actual = run_command_with_stderr do
+        @command.run([])
+      end
+      assert_equal(MISSING_QUERY_LOG_ERROR, actual)
+    end
+
+    def test_too_few_query_logs
+      actual = run_command_with_stderr do
+        @command.run([
+          fixture_path("query1.log")
+        ])
+      end
+      assert_equal(MISSING_QUERY_LOG_ERROR, actual)
+    end
+
+    def test_too_many_query_logs
+      actual = run_command_with_stderr do
+        @command.run([
+          fixture_path("query1.log"),
+          fixture_path("query2.log"),
+          fixture_path("query1.log")
+        ])
+      end
+      assert_equal(MISSING_QUERY_LOG_ERROR, actual)
+    end
+
+    def test_nonexistent_input_old_query
+      actual = run_command_with_stderr do
+        @command.run([
+          fixture_path("nonexsistent.log"),
+          fixture_path("query2.log")
+        ])
+      end
+      assert_equal(<<-OUTPUT, actual)
+query log path doesn't exist: <#{fixture_path("nonexsistent.log")}>
+      OUTPUT
+    end
+
+    def test_nonexistent_input_new_query
+      actual = run_command_with_stderr do
+        @command.run([
+          fixture_path("query1.log"),
+          fixture_path("nonexsistent.log")
+        ])
+      end
+      assert_equal(<<-OUTPUT, actual)
+query log path doesn't exist: <#{fixture_path("nonexsistent.log")}>
+      OUTPUT
+    end
+
+    def test_output
+      Tempfile.open('test_output') do |output|
+        @command.run([
+                       "--slow-query-ratio=0",
+                       "--slow-query-second=0",
+                       "--slow-operation-ratio=0",
+                       "--slow-operation-second=0",
+                       "--output=#{output.path}",
+                       fixture_path("query1.log"),
+                       fixture_path("query2.log")
+                     ])
+        expected = <<-OUTPUT
+Query: select --table Site --limit 0
+  Mean (old): 12.0msec
+  Mean (new): 14.0msec
+  Diff:       +2.0msec/+1.17
+  Operations:
+    Operation[0]: select
+      Mean (old): 5.0msec
+      Mean (new): 6.0msec
+      Diff:       +1.0msec/+1.20
+Summary:
+  Slow queries:    1/1(100.00%)
+  Slow operations: 1/2( 50.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+        assert_equal(expected, output.read)
+      end
+    end
+
+    def test_n_query
+      actual = run_command("--n-entries=1",
+                           "--slow-query-ratio=0",
+                           "--slow-query-second=0",
+                           "--slow-operation-ratio=0",
+                           "--slow-operation-second=0",
+                           fixture_path("nquery.log"),
+                           fixture_path("nquery2.log"))
+      expected = <<-OUTPUT
+Query: select --table Site --filter "_id >= 4 && _id <= 6"
+  Mean (old): 70.0msec
+  Mean (new): 90.0msec
+  Diff:       +20.0msec/+1.29
+  Operations:
+    Operation[0]: filter #<accessor _id(Site)> greater_equal 4
+      Mean (old): 40.0msec
+      Mean (new): 80.0msec
+      Diff:       +40.0msec/+2.00
+    Operation[1]: filter #<accessor _id(Site)> less_equal 6
+      Mean (old): 10.0msec
+      Mean (new): 20.0msec
+      Diff:       +10.0msec/+2.00
+    Operation[2]: select
+      Mean (old): 10.0msec
+      Mean (new): 20.0msec
+      Diff:       +10.0msec/+2.00
+    Operation[3]: output
+      Mean (old): 10.0msec
+      Mean (new): 20.0msec
+      Diff:       +10.0msec/+2.00
+Summary:
+  Slow queries:    1/1(100.00%)
+  Slow operations: 4/4(100.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+
+  sub_test_case(".new") do
+    def test_output
+      actual = run_command("--slow-query-ratio=0.0",
+                           "--slow-query-second=0.0",
+                           "--slow-operation-ratio=0.0",
+                           "--slow-operation-second=0.0",
+                           fixture_path("query1.log"),
+                           fixture_path("query2.log"))
+      expected = <<-OUTPUT
+Query: select --table Site --limit 0
+  Mean (old): 12.0msec
+  Mean (new): 14.0msec
+  Diff:       +2.0msec/+1.17
+  Operations:
+    Operation[0]: select
+      Mean (old): 5.0msec
+      Mean (new): 6.0msec
+      Diff:       +1.0msec/+1.20
+Summary:
+  Slow queries:    1/1(100.00%)
+  Slow operations: 1/2( 50.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+
+  sub_test_case("query-ratio") do
+    def test_filtered
+      actual = run_command("--slow-query-ratio=2",
+                           "--slow-query-second=0",
+                           "--slow-operation-ratio=0",
+                           "--slow-operation-second=0",
+                           fixture_path("query1.log"),
+                           fixture_path("query2.log"))
+      expected = <<-OUTPUT
+Summary:
+  Slow queries:    0/1(  0.00%)
+  Slow operations: 0/0(  0.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+
+  sub_test_case("operation-ratio") do
+    def test_filtered
+      actual = run_command("--slow-query-ratio=0",
+                           "--slow-query-second=0",
+                           "--slow-operation-ratio=2",
+                           "--slow-operation-second=0",
+                           fixture_path("query1.log"),
+                           fixture_path("query2.log"))
+      expected = <<-OUTPUT
+Query: select --table Site --limit 0
+  Mean (old): 12.0msec
+  Mean (new): 14.0msec
+  Diff:       +2.0msec/+1.17
+  Operations:
+Summary:
+  Slow queries:    1/1(100.00%)
+  Slow operations: 0/2(  0.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+
+  sub_test_case("query-second") do
+    def test_filtered
+      actual = run_command("--slow-query-ratio=0",
+                           "--slow-query-second=0.02",
+                           "--slow-operation-ratio=0",
+                           "--slow-operation-second=0",
+                           fixture_path("query1.log"),
+                           fixture_path("query2.log"))
+      expected = <<-OUTPUT
+Summary:
+  Slow queries:    0/1(  0.00%)
+  Slow operations: 0/0(  0.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+
+  sub_test_case("operation-second") do
+    def test_filtered
+      actual = run_command("--slow-query-ratio=0",
+                           "--slow-query-second=0",
+                           "--slow-operation-ratio=0",
+                           "--slow-operation-second=0.001",
+                           fixture_path("query1.log"),
+                           fixture_path("query2.log"))
+      expected = <<-OUTPUT
+Query: select --table Site --limit 0
+  Mean (old): 12.0msec
+  Mean (new): 14.0msec
+  Diff:       +2.0msec/+1.17
+  Operations:
+    Operation[0]: select
+      Mean (old): 5.0msec
+      Mean (new): 6.0msec
+      Diff:       +1.0msec/+1.20
+Summary:
+  Slow queries:    1/1(100.00%)
+  Slow operations: 1/2( 50.00%)
+  Caches (old):    0/1(  0.00%)
+  Caches (new):    0/1(  0.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+
+  sub_test_case("cache") do
+    def test_ignored_cache
+      actual = run_command("--slow-query-ratio=0",
+                           "--slow-operation-ratio=0",
+                           "--slow-query-second=0",
+                           fixture_path("cache.log"),
+                           fixture_path("cache.log"))
+      expected = <<-OUTPUT
+Summary:
+  Slow queries:    0/0(  0.00%)
+  Slow operations: 0/0(  0.00%)
+  Caches (old):    1/1(100.00%)
+  Caches (new):    1/1(100.00%)
+      OUTPUT
+      assert_equal(expected, actual)
+    end
+  end
+end
